796c975fb4952089c08757c69c003338534d74ef
Sylvain Willy <willy.sylvain@gmail.com>
2019-09-07 22:44:48 +0200

    More efficient regex usage for clearing

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,11 @@
 import * as vscode from 'vscode';
 
 // TODO Put this in a config file
-const prefix = 'PUKE-DEBUG: ';
-const debug_function_name = 'fmt.Println';
+const puke_point_format = 'fmt.Println(\'PUKE-POINT: filename: %filename%, line: %line%\')';
 const comment_tag = 'PKDBG';
 const comment_prefix = '//';
-const comment = comment_prefix + comment_tag;
+const comment_sufix = '';
+const comment = ' ' + comment_prefix + ' ' + comment_tag + ' ' + comment_sufix;
 
 // By Mathias Bynens
 // From: https://stackoverflow.com/questions/3115150/how-to-escape-regular-expression-special-characters-using-javascript
@@ -31,9 +31,9 @@ export function activate(context: vscode.ExtensionContext) {
 				filename = filename.substr(rootPath.length);
 			}
 
-			const debug_output = debug_function_name + '("' + prefix + ' filename: ' + filename + ', line: ' + line + '")' + comment;
+			const puke_point = puke_point_format.replace('%filename%', filename).replace('%line%', line.toString()) + comment;
 			editor.edit(function (editBuilder: vscode.TextEditorEdit) {
-				editBuilder.insert(position, debug_output);
+				editBuilder.insert(position, puke_point);
 			});
 
 			// TODO Update line numbers of other debug
@@ -49,20 +49,14 @@ export function activate(context: vscode.ExtensionContext) {
 			const text = editor.document.getText();
 			const regex = new RegExp('^.+'+escapeRegExp(comment) + '$', 'gm');
 
-			console.log('regex', regex);
-			console.log('text', text);
-			const commentMatches = text.match(regex);
-			console.log('matches', commentMatches);
-
-			if (commentMatches) {
-				editor.edit(function(editBuilder: vscode.TextEditorEdit) {
-					commentMatches.forEach(function(match) {
-						const begin = editor.document.positionAt(text.indexOf(match));
-						const end = new vscode.Position(begin.line+1, 0);
-						editBuilder.delete(new vscode.Range(begin, end));
-					});
-				});
-			}
+			let match;
+			editor.edit(function(editBuilder: vscode.TextEditorEdit) {
+				while(match = regex.exec(text)) {
+					const begin = editor.document.positionAt(match.index);
+					const end = new vscode.Position(begin.line+1, 0);
+					editBuilder.delete(new vscode.Range(begin, end));
+				}
+			});
 		}
 	});
 
