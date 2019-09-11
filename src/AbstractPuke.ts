8dd45da9dde5e5feeba510774a235078d8bf20bf
Sylvain Willy <willy.sylvain@gmail.com>
2019-09-11 11:44:47 +0200

    JSDoc commented all methods in AbstractPuke.ts

--- src/AbstractPuke.ts
+++ src/AbstractPuke.ts
@@ -4,30 +4,42 @@ import { runInThisContext } from 'vm';
 
 export abstract class AbstractPukeControler {
     protected commentSubTag: string;
-    protected pukeformat: string;
+    protected pukeFormatKey: string;
 
-    constructor(commentSubTag: string, pukeFormat: string) {
+    constructor(commentSubTag: string, pukeFormatKey: string) {
         this.commentSubTag = commentSubTag;
-        this.pukeformat = pukeFormat;
+        this.pukeFormatKey = pukeFormatKey;
     }
-
+    /**
+     * Builds the comment string at the end of a puke
+     * @returns string A comment string with opening and closing tags
+     */
     protected makeComment(): string {
         const conf = vscode.workspace.getConfiguration('puke-debug');
         const optionalClosing = (' ' + conf.commentClosing).trimRight();
         return `${conf.commentOpening} ${conf.commentTAG}/${this.commentSubTag}` + optionalClosing;
     }
-
+    /**
+     * Fetches the puke format from the settings based on the pukeFormatKey given in constructor and the language
+     * identifier given in parameter
+     * If the pukeFormatKey/languageID does not match any setting, an exception is throwned
+     * @param  {string} languageID The document's language identifier
+     * @returns string A puke format string
+     */
     protected getPukeFormat(languageID: string): string {
+        // languageID is not used by default but may be needed by overriding methods
         const conf = vscode.workspace.getConfiguration('puke-debug');
-
-        if (conf.hasOwnProperty(this.pukeformat)) {
-            return conf[this.pukeformat];
+        if (conf.hasOwnProperty(this.pukeFormatKey)) {
+            return conf[this.pukeFormatKey];
         } else {
-            console.error('makePuke()', 'Wrong setting key in this.pukeformat');
-            return "";
+            throw new Error('Wrong setting key in this.pukeFormatKEy');
         }
     }
-
+    /**
+     * Fetches the print statement format from the settings based on the given language identifier
+     * @param  {string} languageID The document's language identifier
+     * @returns string A print statement format string
+     */
     protected getOutputFormat(languageID: string): string {
         const conf = vscode.workspace.getConfiguration('puke-debug');
         let outputFormat = conf.defaultOutputFormat;
@@ -36,16 +48,44 @@ export abstract class AbstractPukeControler {
         }
         return outputFormat;
     }
-
+    /**
+     * Builds the complete puke statemen with comment based on the pukeFormatKey given in constructor and the language
+     * identifier given in parameter
+     * If the pukeFormatKey/languageID does not match any setting, an exception is throwned
+     * @param  {string} languageID The document's language identifier
+     * @returns string
+     */
     protected makePuke(languageID: string): string {
         const printStatement = this.getOutputFormat(languageID).replace('%output%', this.getPukeFormat(languageID));
         const comment = this.makeComment();
         return `${printStatement} ${comment}`;
     }
-
+    /**
+     * Hook for child class to override
+     * This hook is execuded ONCE before a batch of inserts. It can be used to format a puke with a parameter that
+     * does not depend on the line
+     * @param  {vscode.TextEditor} editor An instance of vscode.TextEditor
+     * @param  {string} puke The puke string to format
+     * @returns string The formated puke string
+     */
     protected hookBeforeAllInsert(editor: vscode.TextEditor, puke: string): string { return puke; }
+
+    
+    /**
+     * Hook for child class to override
+     * This hook is execuded before EACH insert. It can be used to format a puke with a parameter that depends on the line
+     * @param  {vscode.TextEditor} editor An instance of vscode.TextEditor
+     * @param  {vscode.TextLine} selectedLine The line after which the puke will be inserted
+     * @param  {string} puke The puke string to format
+     * @returns string The formated puke string
+     */
     protected hookBeforeEachInsert(editor: vscode.TextEditor, selectedLine: vscode.TextLine, puke: string): string { return puke; }
 
+    /**
+     * Inserts a puke after each line with a cursor carret
+     * @param  {vscode.TextEditor} editor An instance of vscode.TextEditor
+     * @returns Thenable A promise that resolves with a value indicating if the edits could be applied.
+     */
     public insert(editor: vscode.TextEditor): Thenable<boolean> {
         let puke = this.hookBeforeAllInsert(editor, this.makePuke(editor.document.languageId));
         let self = this;
@@ -61,6 +101,12 @@ export abstract class AbstractPukeControler {
         });
     }
 
+    /**
+     * Updates all pukes of this type in the document
+     * A line that won't change is not replaced.
+     * @param  {vscode.TextEditor} editor An instance of vscode.TextEditor
+     * @returns Thenable A promise that resolves with a value indicating if the edits could be applied.
+     */
     public updateAll(editor: vscode.TextEditor): Thenable<boolean> {
         const text = editor.document.getText();
         let puke = this.hookBeforeAllInsert(editor, this.makePuke(editor.document.languageId));
@@ -86,6 +132,11 @@ export abstract class AbstractPukeControler {
         });
     }
 
+    /**
+     * Removes all pukes of this type in the document
+     * @param  {vscode.TextEditor} editor An instance of vscode.TextEditor
+     * @returns Thenable A promise that resolves with a value indicating if the edits could be applied.
+     */
     public clearAll(editor: vscode.TextEditor): Thenable<boolean> {
         const text = editor.document.getText();
         // Matches all lines ending with the puke-point comment
