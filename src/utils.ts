b0315d0254d286daf2844214e48b326c34605a08
Sylvain Willy <willy.sylvain@gmail.com>
2020-06-22 23:27:20 +0200

    Remove unused utils functions

--- src/utils.ts
+++ src/utils.ts
@@ -15,21 +15,3 @@ export function currentFileName(document: vscode.TextDocument): string {
     return filename;
 }
 
-export function indentationOfLine(document: vscode.TextDocument, line: number): string {
-    const currntLine = document.lineAt(line);
-    return currntLine.text.substr(0, currntLine.firstNonWhitespaceCharacterIndex);
-}
-
-export function make_comment(): string {
-	const conf = vscode.workspace.getConfiguration('puke-debug');
-	return conf.commentOpening + ' ' + conf.commentTAG + (' ' + conf.commentClosing).trimRight();
-}
-
-export function outputFormat(languageID: string): string {
-    const conf = vscode.workspace.getConfiguration('puke-debug');
-    let outputFormat = conf.defaultOutputFormat;
-    if (languageID !== "" && conf.outputFormats.hasOwnProperty(languageID)) {
-        outputFormat = conf.outputFormats[languageID];
-    }
-    return outputFormat;
-}
